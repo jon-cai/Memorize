80e8c8a5ecae202877130fec73ac4218243512ec
Jonathan Cai <jonthncai@gmail.com>
2024-03-12 17:37:11 -0700

    Initial commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jonathan Cai on 3/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis: [String] = ["🐰", "🐶", "🐻", "🐹", "🦄", "🐧", "🐷", "🐯", "🦌", "🐢", "🐳", "🦖"]
+    
+    @State var cardCount: Int = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+}
+
+struct CardView: View {
+    // Views are immutable, but body is can be changing and can be called and re-evaluated all the time
+    // @State is a pointer to the value since pointer doesn't change but what it points to does
+    // @State is temporary state (displaying only, not about actual app)
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jonathan Cai on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
